1fccb4c1a83ee5f2ea7aaf7e0a4ca47119848990
Nikhil <nikhilnainwaya17@gmail.com>
2024-05-20 23:51:09 +0530

    completed teh homapage with lazy load, added tailwind css files and componets

--- js/products.js
+++ js/products.js
@@ -4,6 +4,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import {getFirestore, collection, getDocs, doc, setDoc, addDoc} from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js'
 
 
+console.log('test');
+
+
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAfvkCmr4DiHoqnojqpw98iQOChgHkVn9Y",
     authDomain: "shopsphere-67451.firebaseapp.com",
@@ -18,6 +24,144 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
+
+
+
+let products = [];
+let currentIdx = 0;
+const limit = 12;
+
+const trendingProductContainer = document.querySelector(".trending-products");
+const loadingIndicator = document.querySelector("#loadingIndicator");
+// loadingIndicator.style.display = "none"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('./js/product.json')
+        .then(response => response.json())
+        .then((data) => {
+            products = shuffleArray(data);
+            loadProducts();
+            window.addEventListener('scroll', scrollHandle);
+        })
+        .catch(e => console.log('Could not fetch the product.json', e))
+});
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+
+function loadProducts() {
+    const nextIndex = currentIdx + limit;
+    const productToLoad = products.slice(currentIdx, nextIndex);
+
+    productToLoad.forEach(e => {
+
+        const product = getProduct(e);
+
+        trendingProductContainer.innerHTML = trendingProductContainer.innerHTML + product;
+
+    });
+
+    currentIdx = nextIndex;
+    loadingIndicator.style.display = 'none';
+    
+}
+
+function scrollHandle() {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && currentIdx < products.length) {
+        window.removeEventListener('scroll', scrollHandle);
+        loadingIndicator.style.display = 'block';
+        setTimeout(() => {
+            loadProducts();
+            window.addEventListener('scroll', scrollHandle);
+        }, 500);
+    }
+}
+
+
+
+function getProduct(proObject) {
+    return `<div class="product-card w-full bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700">
+    <a href="#">
+        <img class="p-2 mx-auto rounded-t-lg size-10/12" src=${proObject.thumbnail} alt="product image" />
+    </a>
+    <div class="product-data px-5 pb-5">
+        <a href="#">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-black">${proObject.title}</h5>
+        </a>
+        <div>
+            <div class="flex items-center mt-2.5 mb-5">
+                <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                </div>
+                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">${proObject.rating}</span>
+            </div>
+            <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-gray-900 dark:text-black">$${proObject.price}</span>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View</a>
+            </div>
+        </div>
+    </div>
+    </div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Code to add the json product in the data base {do not uncomment it}
+// console.log(productData[0]);
+
+// const productCollection = collection(db, 'Products');
+// const docRef = addDoc(productCollection, productData[0])
+
+// productData.forEach(e => {
+//     const docRef = addDoc(productCollection, e);
+// })
 
 
 
